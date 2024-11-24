f87f1fa3ef6805c6916b93b7388a7b6d2efb6b55
Ikki <vivekprakash.st@gmail.com>
2024-11-24 22:17:48 +0530

    route-/

--- backend/server/route.py
+++ backend/server/route.py
@@ -30,3 +30,7 @@ def connect():
         return "Error connecting to database", 400
     return "Connected to database", 200
 
+@app.route('/', methods=['GET'])
+@cross_origin()
+def start():
+    return "SQL AI Backend Working", 200
